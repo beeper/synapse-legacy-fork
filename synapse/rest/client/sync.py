e21e39a5666c1a67ec0c12c9ddbdbed6f8866401
Liam Sargent <themailman05@gmail.com>
2022-11-29 12:05:27 -0600

    Only add room preview if we are working with a JoinedSyncResult

--- synapse/rest/client/sync.py
+++ synapse/rest/client/sync.py
@@ -530,7 +530,6 @@ class SyncRestServlet(RestServlet):
         )
 
         account_data = room.account_data
-
         result: JsonDict = {
             "timeline": {
                 "events": serialized_timeline,
@@ -539,11 +538,12 @@ class SyncRestServlet(RestServlet):
             },
             "state": {"events": serialized_state},
             "account_data": {"events": account_data},
-            "com.beeper.inbox.preview": room.preview,
         }
+
 
         if joined:
             assert isinstance(room, JoinedSyncResult)
+            result["com.beeper.inbox.preview"] = room.preview
             ephemeral_events = room.ephemeral
             result["ephemeral"] = {"events": ephemeral_events}
             result["unread_notifications"] = room.unread_notifications
