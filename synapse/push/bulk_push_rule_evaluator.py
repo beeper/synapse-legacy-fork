070097ec7c1b1ba7d942d8286b2e0cf4be7337c9
Nick Mills-Barrett <nick@beeper.com>
2024-01-29 16:10:17 +0000

    Don't include state events in unread_count
    # Conflicts:
    #	synapse/push/bulk_push_rule_evaluator.py

--- synapse/push/bulk_push_rule_evaluator.py
+++ synapse/push/bulk_push_rule_evaluator.py
@@ -75,28 +75,9 @@ push_rules_state_size_counter = Counter(
 )
 
 
-STATE_EVENT_TYPES_TO_MARK_UNREAD = {
-    EventTypes.Topic,
-    EventTypes.Name,
-    EventTypes.RoomAvatar,
-    EventTypes.Tombstone,
-}
-
-
-SENTINEL = object()
-
-
 def _should_count_as_unread(event: EventBase, context: EventContext) -> bool:
     # Exclude rejected and soft-failed events.
     if context.rejected or event.internal_metadata.is_soft_failed():
-        return False
-
-    # Exclude notices.
-    if (
-        not event.is_state()
-        and event.type == EventTypes.Message
-        and event.content.get("msgtype") == "m.notice"
-    ):
         return False
 
     # Exclude edits.
@@ -104,18 +85,13 @@ def _should_count_as_unread(event: EventBase, context: EventContext) -> bool:
     if relates_to and relates_to.rel_type == RelationTypes.REPLACE:
         return False
 
-    # Mark events that have a non-empty string body as unread.
-    body = event.content.get("body")
-    if isinstance(body, str) and body:
-        return True
-
-    # Mark some state events as unread.
-    if event.is_state() and event.type in STATE_EVENT_TYPES_TO_MARK_UNREAD:
-        return True
-
-    # Mark encrypted events as unread.
-    if not event.is_state() and event.type == EventTypes.Encrypted:
-        return True
+    # Mark encrypted and plain text messages events as unread.
+    if not event.is_state():
+        if event.type == EventTypes.Encrypted:
+            return True
+        elif event.type == EventTypes.Message:
+            body = event.content.get("body")
+            return isinstance(body, str) and bool(body)
 
     return False
 
